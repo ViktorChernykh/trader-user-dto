950e7685cd3eb20763bf86e62e83eaad3013a402
Viсtor Chernykh <v-cher9@yandex.ru>
2024-08-24 15:21:54 +0300

    Change Tariff

--- Sources/TraderUserDto/Tariff.swift
+++ Sources/TraderUserDto/Tariff.swift
@@ -10,17 +10,17 @@ public enum Tariff: String, Codable, Sendable {
 	// MARK: Static properties
 	public static let schema = "tariff"
 
-	case tariff1
-	case tariff2
-	case tariff3
-	case tariff4
+	case free
+	case start
+	case business
+	case enterprise
 
 	public var level: Int {
 		switch self {
-		case .tariff1: 1
-		case .tariff2: 2
-		case .tariff3: 3
-		case .tariff4: 4
+		case .free: 0
+		case .start: 1
+		case .business: 2
+		case .enterprise: 3
 		}
 	}
 }
