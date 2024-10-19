1f917272f55ffd88a300345d19a9a407f172e0e4
Viсtor Chernykh <v-cher9@yandex.ru>
2024-10-19 16:47:53 +0300

    Add userId to TokenDto

--- Sources/TraderUserDto/Response/TokenDto.swift
+++ Sources/TraderUserDto/Response/TokenDto.swift
@@ -5,15 +5,20 @@
 //  Created by Victor Chernykh on 12.07.2022.
 //
 
-import Foundation
+import struct Foundation.UUID
 
 public struct TokenDto: Codable, Sendable {
 
     // MARK: Stored properties
     public let access: String
+    public let userId: UUID
 
     // MARK: - Init
-    public init(access: String) {
+    public init(
+		access: String,
+		userId: UUID
+	) {
         self.access = access
+        self.userId = userId
     }
 }
